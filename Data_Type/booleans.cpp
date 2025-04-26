5d036997fc595fc04e498478f6c5da9a6093f64d
MAC <mac@MACs-MacBook-Pro-2.local>
2025-04-26 13:50:54 +0100

    feat: completed the C++ data type class

---
+++ Data_Type/booleans.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    bool red = true;
+    bool green = false;
+
+  
+
+    if (red == true){
+        cout << "The traffic light is red." << endl;
+    }else {
+        cout << "The traffic light is green." << endl;
+    }
+}
---
+++ Data_Type/comments.cpp
@@ -0,0 +1,20 @@
+
+// this brings in the standard input/output library (iostream)
+#include <iostream>
+
+// this brings in the standard namespace
+using namespace std;
+
+
+int main() {
+    // this is a comment
+    cout << "Hello, world!" << endl; // this is another comment
+
+    // this shows the end of the program
+    return 0;
+}
+ 
+/*
+this is a multi-line comment
+
+*/
